e337abb7a33fe6e795c981608e90ae7ca548f294
Dionisio <dionidukart@gmail.com>
2024-09-19 17:34:03 -0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const express= require("express");
-const app= express();
+import express from "express";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer";
+import handlebars from "handlebars";
+
+const app = express();
+dotenv.config();
+
+puerto
+app.listen(puerto, () => {
+    console.log(`Hola puerto ${puerto}!`);
+})
+
+app.get("/", (req, res) => {
+    res.status(200).send({estado:true});
+})
