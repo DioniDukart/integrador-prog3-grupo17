82a2cc674a79d45625784cac7e123345db5389bc
WilliamGaiga <willygaiga17@gmail.com>
2024-10-07 18:19:52 -0300

    solicitud rutas reclamos
    Se agregan otras solicitudes http en reclamosRutas.js

--- v1/rutas/reclamosRutas.js
+++ v1/rutas/reclamosRutas.js
@@ -7,11 +7,10 @@ const reclamosControlador = new ReclamosControlador();
 
 router.get("/reclamos", reclamosControlador.buscarTodos);
 router.get("/reclamos/:idReclamo", reclamosControlador.buscarPorId);
-/* 
-router.put();
-router.post();
-router.patch();
-router.delete();
-*/
+router.post("/reclamos", reclamosControlador.crear);
+router.put("/reclamos/:idReclamo", reclamosControlador.actualizar);
+router.patch("/reclamos/:idReclamo", reclamosControlador.actualizarParcialmente);
+router.delete("/reclamos/:idReclamo", reclamosControlador.eliminar);
+
 
 export { router };
