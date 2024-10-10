90ea2c5bf3c9bf2a9c1ba729c4fd222fac5bf83d
WilliamGaiga <willygaiga17@gmail.com>
2024-10-10 17:59:44 -0300

    se agrega 4 archivos
    agrego oficinasRutas, oficinasServicios, oficinasControlador, y oficinasBD

--- v1/rutas/oficinasRutas.js
+++ v1/rutas/oficinasRutas.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 const oficinasControlador = new OficinasControlador();
 
-router.get("/oficinas", oficinasControlador.buscarTodas);
-router.get("/oficinas/:idOficina", oficinasControlador.buscarPorId);
-router.post("/oficinas", oficinasControlador.crear);
-router.put("/oficinas/:idOficina", oficinasControlador.actualizar);
-router.patch("/oficinas/:idOficina", oficinasControlador.actualizarParcialmente);
-router.delete("/oficinas/:idOficina", oficinasControlador.eliminar);
-
+router.post("/oficinas", oficinasControlador.crear);  
+router.get("/oficinas", oficinasControlador.buscarTodas);  
+router.get("/oficinas/:idOficina", oficinasControlador.buscarPorId);  
+router.put("/oficinas/:idOficina", oficinasControlador.actualizar);  
+router.patch("/oficinas/:idOficina", oficinasControlador.actualizarParcial); 
+router.delete("/oficinas/:idOficina", oficinasControlador.eliminar);  
 
 export { router };
+
